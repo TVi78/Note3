9a751df1646c2754e24bf3d76aaa5ca496dc9213
Виктория Тарасова <viky.ne2@gmail.com>
2021-08-31 23:15:28 +0300

    Девятый урок 1. Добавление элемента и очищение всего поля

--- app/src/main/java/com/zadania/note3/data/CardsSource.java
+++ app/src/main/java/com/zadania/note3/data/CardsSource.java
@@ -4,6 +4,9 @@ import com.zadania.note3.data.CardData;
 
 public interface CardsSource {
     CardData getCardData(int position);
-
     int size();
+    void deleteCardData(int position);
+    void updateCardData(int position, CardData cardData);
+    void addCardData(CardData cardData);
+    void clearCardData();
 }
--- app/src/main/java/com/zadania/note3/ui/NoteFragment.java
+++ app/src/main/java/com/zadania/note3/ui/NoteFragment.java
@@ -2,20 +2,28 @@ package com.zadania.note3.ui;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.zadania.note3.R;
+import com.zadania.note3.data.CardData;
 import com.zadania.note3.data.CardsSource;
 import com.zadania.note3.data.CardsSourceImpl;
 
 public class NoteFragment extends Fragment {
+    private CardsSource data;
+    private NoteAdapter adapter;
+    private RecyclerView recyclerView;
 
     public static NoteFragment newInstance() {
         return new NoteFragment();
@@ -27,12 +35,43 @@ public class NoteFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_notes2, container, false);
         RecyclerView recyclerView = view.findViewById(R.id.recycler_view_lines);
-        CardsSource data = new CardsSourceImpl(getResources()).init();
-        initRecyclerView(recyclerView, data);
+        data = new CardsSourceImpl(getResources()).init();
+        initView(view);
+        setHasOptionsMenu(true);
         return view;
     }
 
-    private void initRecyclerView(RecyclerView recyclerView, CardsSource data) {
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.cards_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                data.addCardData(new CardData("Заметка " + (data.size()+1),
+                        "Название ","Описание ","Дата", R.drawable.note4,
+                        false));
+                adapter.notifyItemInserted(data.size() - 1);
+                recyclerView.scrollToPosition(data.size() - 1);
+                return true;
+            case R.id.action_clear:
+                data.clearCardData();
+                adapter.notifyDataSetChanged();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initView(View view) {
+        recyclerView = view.findViewById(R.id.recycler_view_lines);
+        // Получим источник данных для списка
+        data = new CardsSourceImpl(getResources()).init();
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
 
         // Эта установка служит для повышения производительности системы
         recyclerView.setHasFixedSize(true);
@@ -42,7 +81,7 @@ public class NoteFragment extends Fragment {
         recyclerView.setLayoutManager(layoutManager);
 
         // Установим адаптер
-        final NoteAdapter adapter = new NoteAdapter(data);
+        adapter = new NoteAdapter(data);
         recyclerView.setAdapter(adapter);
 
         // Добавим разделитель карточек
